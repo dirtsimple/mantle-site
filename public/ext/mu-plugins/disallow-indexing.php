7ae1d8fdf681fca70845a544569cf41bab5a3288
PJ Eby <pje@telecommunity.com>
2019-04-21 22:32:24 -0400

    Fix blocking indexing of 'prod' sites

--- public/ext/mu-plugins/disallow-indexing.php
+++ public/ext/mu-plugins/disallow-indexing.php
@@ -3,12 +3,12 @@
 Plugin Name:  Disallow Indexing
 Plugin URI:   https://roots.io/bedrock/
 Description:  Disallow indexing of your site on non-production environments.
-Version:      1.0.0
+Version:      1.0.1
 Author:       Roots
 Author URI:   https://roots.io/
 License:      MIT License
 */
 
-if (WP_ENV !== 'production' && !is_admin()) {
+if (WP_ENV !== 'production' && WP_ENV !== 'prod' && !is_admin()) {
     add_action('pre_option_blog_public', '__return_zero');
 }
